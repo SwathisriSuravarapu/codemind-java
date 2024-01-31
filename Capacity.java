e5cc8e6f3c8057f96fce206b973149042479a128
SwathisriSuravarapu <119840037+SwathisriSuravarapu@users.noreply.github.com>
2024-01-31 20:48:39 +0530

    import java.util.*; public class A{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int b= sc.nextInt();         int c= sc.nextInt();         int d = a*b*c;         System.out.print(d+" KB");     } }

---
+++ Capacity.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class A{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b= sc.nextInt();
+        int c= sc.nextInt();
+        int d = a*b*c;
+        System.out.print(d+" KB");
+    }
+}
